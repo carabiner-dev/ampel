dada776ea1a70e894495ee29eec76a42030b678e
Adolfo García Veytia (Puerco) <adolfo.garcia@uservers.net>
2025-01-14 21:06:36 -0600

    Expose context data to evaluation engine
    Signed-off-by: Adolfo García Veytia (Puerco) <adolfo.garcia@uservers.net>

---
+++ pkg/api/v1/context.go
@@ -0,0 +1,15 @@
+package v1
+
+// ToMap compiles the context data values into a map, filling the fields
+// with their defaults when needed.
+func (c *Context) ToMap() map[string]any {
+	ret := map[string]any{}
+	for label, value := range c.Values {
+		if value.Value != nil {
+			ret[label] = value.Value.AsInterface()
+		} else {
+			ret[label] = value.Default.AsInterface()
+		}
+	}
+	return ret
+}
