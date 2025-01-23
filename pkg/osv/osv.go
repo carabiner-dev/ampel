fa59113e415277990bd4dc16b17618c52cd6f0af
Adolfo García Veytia (Puerco) <adolfo.garcia@uservers.net>
2025-01-22 18:00:02 -0600

    Alias all OSV types
    Signed-off-by: Adolfo García Veytia (Puerco) <adolfo.garcia@uservers.net>

--- pkg/osv/osv.go
+++ pkg/osv/osv.go
@@ -2,8 +2,22 @@ package osv
 
 import "github.com/puerco/ampel/pkg/osv/v1_6_7"
 
-// The osv.Record type is always an alias to the latest defined record,
-// however the parser should return the correct type
-type Record = v1_6_7.Record
+const (
+	Version = "v1.6.7"
+)
 
-type RecordList []*Record
+// We maintain aliases of the latest proto generated versions.
+// This ensures any new record gets generated with the latest revision
+// but keeps the older versions still available.
+type (
+	Affected    = v1_6_7.Affected
+	Award       = v1_6_7.Award
+	Credit      = v1_6_7.Credit
+	CWE         = v1_6_7.CWE
+	Range       = v1_6_7.Range
+	Range_Event = v1_6_7.Range_Event
+	Record      = v1_6_7.Record
+	Reference   = v1_6_7.Reference
+	Severity    = v1_6_7.Severity
+	Package     = v1_6_7.Package
+)
