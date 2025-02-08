66c5548edeceac5e857c5f2a58adf5633b7f270c
Adolfo García Veytia (Puerco) <adolfo.garcia@uservers.net>
2025-02-07 21:25:05 -0600

    Set proto flags in intoto pred srtuct
    Signed-off-by: Adolfo García Veytia (Puerco) <adolfo.garcia@uservers.net>

--- pkg/formats/statement/intoto/statement.go
+++ pkg/formats/statement/intoto/statement.go
@@ -42,7 +42,7 @@ func NewStatement(opts ...StatementOption) *Statement {
 }
 
 type Statement struct {
-	PredicateType attestation.PredicateType `json:"predicateType"`
+	PredicateType attestation.PredicateType `json:"predicateType" protobuf:"bytes,3,opt,name=predicate_type,json=predicateType,proto3"`
 	Predicate     attestation.Predicate     `json:"predicate"`
 	gointoto.Statement
 }
