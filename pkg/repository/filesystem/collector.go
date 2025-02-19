a9a6a84480088fdd309cbe8c1144ff767cd133c5
Adolfo García Veytia (Puerco) <adolfo.garcia@uservers.net>
2025-02-18 19:08:50 -0600

    Add initial filesystem collector
    Signed-off-by: Adolfo García Veytia (Puerco) <adolfo.garcia@uservers.net>

--- pkg/repository/filesystem/collector.go
+++ pkg/repository/filesystem/collector.go
@@ -6,37 +6,57 @@ package filesystem
 
 import (
 	"bytes"
+	"context"
 	"errors"
 	"fmt"
 	"io/fs"
+	"path/filepath"
+	"slices"
+	"strings"
 
 	"github.com/carabiner-dev/ampel/pkg/attestation"
-	"github.com/carabiner-dev/ampel/pkg/collector/filter"
 	"github.com/carabiner-dev/ampel/pkg/formats/envelope"
 )
 
 func New(iofs fs.FS) *Collector {
 	return &Collector{
-		FS: iofs,
+		Extensions:       []string{"json", "jsonl", "spdx", "cdx", "bundle"},
+		IgnoreOtherFiles: true,
+		FS:               iofs,
 	}
 }
+
+var _ attestation.Fetcher = (*Collector)(nil)
 
 // Collector is the filesystem collector
 type Collector struct {
-	FS fs.FS
+	Extensions       []string
+	IgnoreOtherFiles bool
+	FS               fs.FS
 }
 
 // Fetch queries the repository and retrieves any attestations matching the query
-func (c *Collector) Fetch(*filter.AttestationQuery) ([]attestation.Envelope, error) {
+func (c *Collector) Fetch(ctx context.Context, opts attestation.FetchOptions) ([]attestation.Envelope, error) {
 	if c.FS == nil {
 		return nil, errors.New("collector has no filesystem defined")
 	}
+
 	ret := []attestation.Envelope{}
 
 	// Walk the filesystem and read any attestations
 	if err := fs.WalkDir(c.FS, ".", func(path string, d fs.DirEntry, err error) error {
+		if d.IsDir() {
+			return nil
+		}
 		if err != nil {
 			return err
+		}
+
+		if c.IgnoreOtherFiles {
+			ext := filepath.Ext(path)
+			if !slices.Contains(c.Extensions, strings.TrimPrefix(ext, ".")) {
+				return nil
+			}
 		}
 
 		// Read the file data from the filesystem
@@ -50,6 +70,10 @@ func (c *Collector) Fetch(*filter.AttestationQuery) ([]attestation.Envelope, err
 		if err != nil {
 			return fmt.Errorf("parsing file: %w", err)
 		}
+
+		if opts.Query != nil {
+			attestations = opts.Query.Run(attestations)
+		}
 		ret = append(ret, attestations...)
 
 		return nil
@@ -59,7 +83,9 @@ func (c *Collector) Fetch(*filter.AttestationQuery) ([]attestation.Envelope, err
 	return ret, nil
 }
 
-// FetchObjectStatements
-func (c *Collector) FetchObjectStatements(attestation.Subject) ([]attestation.Envelope, error) {
-	return nil, nil
+func (c *Collector) FetchAttestationsBySubject(context.Context, attestation.FetchOptions, []attestation.Subject) ([]attestation.Envelope, error) {
+	return nil, attestation.ErrFetcherMethodNotImplemented
+}
+func (c *Collector) FetchAttestationsByPredicateType(context.Context, attestation.FetchOptions, attestation.PredicateType) ([]attestation.Envelope, error) {
+	return nil, attestation.ErrFetcherMethodNotImplemented
 }
