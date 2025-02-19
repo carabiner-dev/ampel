62d46e1b5a2803b9ea7f98be847f7fc408f5eb4a
Adolfo García Veytia (Puerco) <adolfo.garcia@uservers.net>
2025-02-18 19:58:17 -0600

    Add fs collector tests and data
    Signed-off-by: Adolfo García Veytia (Puerco) <adolfo.garcia@uservers.net>

--- pkg/repository/filesystem/collector_test.go
+++ pkg/repository/filesystem/collector_test.go
@@ -9,6 +9,7 @@ import (
 	"testing"
 
 	"github.com/carabiner-dev/ampel/pkg/attestation"
+	"github.com/carabiner-dev/ampel/pkg/filters"
 	"github.com/stretchr/testify/require"
 )
 
@@ -22,8 +23,8 @@ func TestFetch(t *testing.T) {
 		mustErr     bool
 	}{
 		{"all-default", nil, 2, true, false},
-		{"all-default", []string{"spdx"}, 1, true, false},
-		{"all-default", []string{"spdx"}, 2, false, false},
+		{"ext-spdx", []string{"spdx"}, 1, true, false},
+		{"ext-spdx-no-ignore", []string{"spdx"}, 2, false, false},
 	} {
 		t.Run(tc.name, func(t *testing.T) {
 			t.Parallel()
@@ -33,6 +34,43 @@ func TestFetch(t *testing.T) {
 				collector.Extensions = tc.exts
 			}
 			atts, err := collector.Fetch(context.Background(), attestation.FetchOptions{})
+			require.NoError(t, err)
+			require.Len(t, atts, tc.expect)
+		})
+	}
+}
+
+func TestFetchFetchByPredicateType(t *testing.T) {
+	t.Parallel()
+	for _, tc := range []struct {
+		name    string
+		expect  int
+		pt      string
+		opts    attestation.FetchOptions
+		mustErr bool
+	}{
+		{"pt-ok", 1, "https://spdx.dev/Document", attestation.FetchOptions{}, false},
+		{"pt-bad", 0, "something-else", attestation.FetchOptions{}, false},
+		{"pt-ok-with-synth-always", 1, "https://spdx.dev/Document", attestation.FetchOptions{
+			Query: &attestation.Query{
+				Filters: []attestation.Filter{&filters.AlwaysMatch{}},
+			},
+		}, false},
+		{"pt-ok-with-synth-never", 0, "https://spdx.dev/Document", attestation.FetchOptions{
+			Query: &attestation.Query{
+				Filters: []attestation.Filter{&filters.NeverMatch{}},
+			},
+		}, false},
+	} {
+		t.Run(tc.name, func(t *testing.T) {
+			t.Parallel()
+			collector := New(os.DirFS("testdata"))
+
+			atts, err := collector.FetchByPredicateType(
+				context.Background(),
+				tc.opts,
+				attestation.PredicateType(tc.pt),
+			)
 			require.NoError(t, err)
 			require.Len(t, atts, tc.expect)
 		})
