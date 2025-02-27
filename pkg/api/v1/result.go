a54705636a6d69344d7e49148c1a6d1924b4a8d2
Adolfo García Veytia (Puerco) <puerco@carabiner.dev>
2025-02-27 04:45:32 -0600

    Custom marshalers for result dates
    Signed-off-by: Adolfo García Veytia (Puerco) <puerco@carabiner.dev>

---
+++ pkg/api/v1/result.go
@@ -0,0 +1,48 @@
+// SPDX-FileCopyrightText: Copyright 2025 Carabiner Systems, Inc
+// SPDX-License-Identifier: Apache-2.0
+
+package v1
+
+import (
+	"encoding/json"
+)
+
+func (r *Result) MarshalJSON() ([]byte, error) {
+	type Alias Result
+	var start, end string
+	if r.DateStart != nil {
+		start = r.DateStart.AsTime().Format("2006-01-02T15:04:05.000Z")
+	}
+	if r.DateEnd != nil {
+		end = r.DateEnd.AsTime().Format("2006-01-02T15:04:05.000Z")
+	}
+
+	return json.Marshal(
+		&struct {
+			DateStart string `json:"date_start"`
+			DateEnd   string `json:"date_end"`
+			*Alias
+		}{
+			DateStart: start,
+			DateEnd:   end,
+			Alias:     (*Alias)(r),
+		},
+	)
+}
+
+func (er *EvalResult) MarshalJSON() ([]byte, error) {
+	type Alias EvalResult
+	var date string
+	if er.Date != nil {
+		date = er.Date.AsTime().Format("2006-01-02T15:04:05.000Z")
+	}
+	return json.Marshal(
+		&struct {
+			Date string `json:"date"`
+			*Alias
+		}{
+			Date:  date,
+			Alias: (*Alias)(er),
+		},
+	)
+}
