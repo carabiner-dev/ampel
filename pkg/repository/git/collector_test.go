2abd05fe8f080a2ea484911b986e9a02279e8165
Adolfo Garcia Veytia (puerco) <puerco@carabiner.dev>
2025-03-15 15:40:05 -0600

    Collector: Add git repository driver
    Signed-off-by: Adolfo Garcia Veytia (puerco) <puerco@carabiner.dev>

---
+++ pkg/repository/git/collector_test.go
@@ -0,0 +1,52 @@
+// SPDX-FileCopyrightText: Copyright 2025 Carabiner Systems, Inc
+// SPDX-License-Identifier: Apache-2.0
+
+package git
+
+import (
+	"context"
+	"path/filepath"
+	"testing"
+
+	"sigs.k8s.io/release-utils/tar"
+
+	"github.com/carabiner-dev/ampel/pkg/attestation"
+	"github.com/sirupsen/logrus"
+	"github.com/stretchr/testify/require"
+)
+
+func TestClone(t *testing.T) {
+	t.Parallel()
+
+	// unpack test repository to tmp
+	dir := t.TempDir()
+	require.NoError(t, tar.Extract("testdata/repo.tar.gz", dir))
+	logrus.Info(filepath.Join(dir, "repo"))
+	for _, tc := range []struct {
+		name        string
+		init        string
+		expectedNum int
+		mustErr     bool
+	}{
+		{"root", filepath.Join(dir, "repo"), 3, false},
+		{"subpath", filepath.Join(dir, "repo") + "#sboms", 2, false},
+	} {
+		t.Run(tc.name, func(t *testing.T) {
+			t.Parallel()
+			c, err := New(WithLocator(tc.init))
+			require.NoError(t, err)
+
+			err = c.clone()
+			if tc.mustErr {
+				require.Error(t, err)
+				return
+			}
+			require.NoError(t, err)
+
+			// Check if the methods work
+			res, err := c.Fetch(context.Background(), attestation.FetchOptions{})
+			require.NoError(t, err, err)
+			require.Len(t, res, tc.expectedNum)
+		})
+	}
+}
---
+++ pkg/repository/git/options.go
@@ -0,0 +1,51 @@
+// SPDX-FileCopyrightText: Copyright 2025 Carabiner Systems, Inc
+// SPDX-License-Identifier: Apache-2.0
+
+// Pacakge jsonl implements an attestations collector that reads
+// from files using the JSON Lines (jsonl) format.
+
+package git
+
+import (
+	"fmt"
+	"net/url"
+	"strings"
+)
+
+type optFn = func(*Options) error
+
+type Options struct {
+	URL    string
+	Path   string
+	Ref    string
+	Commit string
+}
+
+var defaultOptions = Options{}
+
+func (o *Options) Validate() error {
+	return nil
+}
+
+func WithLocator(init string) optFn {
+	return func(opts *Options) error {
+		u, err := url.Parse(init)
+		if err != nil {
+			return fmt.Errorf("parsing url: %w", err)
+		}
+		path, branch, ok := strings.Cut(u.Path, "@")
+		opts.URL = u.Scheme + u.Hostname() + path
+		if ok {
+			opts.Ref = branch
+		}
+		opts.Path = u.Fragment
+		return nil
+	}
+}
+
+func WithPath(path string) optFn {
+	return func(opts *Options) error {
+		opts.Path = path
+		return nil
+	}
+}
