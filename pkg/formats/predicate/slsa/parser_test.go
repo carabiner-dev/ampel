0b7d974640910092f60a2fcbf24a22ee0b6ba75c
Adolfo García Veytia (Puerco) <puerco@carabiner.dev>
2025-03-20 20:04:34 -0600

    Correct SLSA v0.2 parameters field
    Somewhere we picked up a slsa proto defining the parameters
    field in the invocation as a list of strings. It should be an
    object. This corrects it.
    
    Signed-off-by: Adolfo García Veytia (Puerco) <puerco@carabiner.dev>

--- pkg/formats/predicate/slsa/parser_test.go
+++ pkg/formats/predicate/slsa/parser_test.go
@@ -14,15 +14,22 @@ func TestParseV02(t *testing.T) {
 	t.Parallel()
 
 	// This is just a simple test that ensures the parser can, well, parse
-	p := ParserV02{}
-	data, err := os.ReadFile("testdata/sample-v0.2.json")
-	require.NoError(t, err)
-	res, err := p.Parse(data)
-	require.NoError(t, err)
-	require.NotNil(t, res)
+	t.Run("slsa-v02-pass", func(t *testing.T) {
+		t.Parallel()
+		p := ParserV02{}
+		data, err := os.ReadFile("testdata/sample-v0.2.json")
+		require.NoError(t, err)
+		res, err := p.Parse(data)
+		require.NoError(t, err)
+		require.NotNil(t, res)
+	})
 
-	data, err = os.ReadFile("testdata/bad-v0.2.json")
-	require.NoError(t, err)
-	_, err = p.Parse(data)
-	require.Error(t, err)
+	t.Run("slsa-v02-fail", func(t *testing.T) {
+		t.Parallel()
+		p := ParserV02{}
+		data, err := os.ReadFile("testdata/bad-v0.2.json")
+		require.NoError(t, err)
+		_, err = p.Parse(data)
+		require.Error(t, err)
+	})
 }
