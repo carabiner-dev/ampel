379887f85310ee924bdd2faaa37c1c3d668fe3f6
Adolfo Garcia Veytia (puerco) <puerco@carabiner.dev>
2025-04-08 22:28:57 -0400

    Initial checkin of protobom plugin
    Signed-off-by: Adolfo Garcia Veytia (puerco) <puerco@carabiner.dev>

---
+++ pkg/evaluator/plugins/protobom/protobom.go
@@ -0,0 +1,64 @@
+// SPDX-FileCopyrightText: Copyright 2025 Carabiner Systems, Inc
+// SPDX-License-Identifier: Apache-2.0
+
+package protobom
+
+import (
+	"bytes"
+
+	api "github.com/carabiner-dev/ampel/pkg/api/v1"
+	"github.com/carabiner-dev/ampel/pkg/attestation"
+	"github.com/carabiner-dev/ampel/pkg/evaluator/class"
+	"github.com/carabiner-dev/ampel/pkg/formats/predicate/cyclonedx"
+	"github.com/carabiner-dev/ampel/pkg/formats/predicate/spdx"
+	"github.com/google/cel-go/cel"
+	"github.com/protobom/cel/pkg/elements"
+	"github.com/protobom/cel/pkg/library"
+	"github.com/protobom/protobom/pkg/reader"
+	"github.com/sirupsen/logrus"
+)
+
+func New() *Plugin {
+	return &Plugin{}
+}
+
+type Plugin struct {
+}
+
+func (h *Plugin) Capabilities() []api.Capability {
+	return []api.Capability{
+		api.CapabilityEvalEnginePlugin,
+	}
+}
+
+func (p *Plugin) CanRegisterFor(c class.Class) bool {
+	return c.Name() == "cel"
+}
+
+func (p *Plugin) Library() cel.EnvOption {
+	return library.NewProtobom().EnvOption()
+}
+
+func (p *Plugin) VarValues(_ *api.Policy, _ attestation.Subject, preds []attestation.Predicate) map[string]any {
+	sbomList := []any{}
+	r := reader.New()
+	logrus.Debugf("Inserting protobom vars (from %d predicates)", len(preds))
+	for _, pred := range preds {
+		if pred.GetType() != spdx.PredicateType && pred.GetType() != cyclonedx.PredicateType {
+			continue
+		}
+		doc, err := r.ParseStream(bytes.NewReader(pred.GetData()))
+		if err != nil {
+			// we cannot return errs so..
+			continue
+		}
+		sbomList = append(sbomList, &elements.Document{
+			Document: doc,
+		})
+	}
+
+	return map[string]any{
+		"protobom": elements.Protobom{},
+		"sboms":    sbomList,
+	}
+}
