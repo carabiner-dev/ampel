5f3b9c5e2a71c160cac569863eee552bc0b0ebfc
Adolfo García Veytia (Puerco) <puerco@carabiner.dev>
2025-05-10 12:45:49 -0600

    Remove composing functions from policy parser
    Since we now have a dedicated compiler, the policy parser will from now on
    only parser json. This commit removes the functions from the parser that
    pulled remote data and recomposed policies.
    
    Signed-off-by: Adolfo García Veytia (Puerco) <puerco@carabiner.dev>

--- pkg/policy/implementation.go
+++ pkg/policy/implementation.go
@@ -4,30 +4,20 @@
 package policy
 
 import (
-	"bytes"
-	"crypto/sha256"
-	"errors"
 	"fmt"
-	"maps"
-	"strings"
 
-	"github.com/carabiner-dev/hasher"
-	"github.com/sirupsen/logrus"
 	"google.golang.org/protobuf/encoding/protojson"
-	"google.golang.org/protobuf/proto"
 
 	v1 "github.com/carabiner-dev/ampel/pkg/api/v1"
 )
 
 type parserImplementation interface {
 	ParsePolicySet([]byte) (*v1.PolicySet, error)
-	FetchReferences(PolicyFetcher, *v1.PolicySet) (*policyStore, error)
-	CompletePolicySet(*v1.PolicySet, *policyStore) error
 }
 
-type defaultParserImplementation struct{}
+type defaultParserImplementationV1 struct{}
 
-func (dpi *defaultParserImplementation) ParsePolicySet(policySetData []byte) (*v1.PolicySet, error) {
+func (dpi *defaultParserImplementationV1) ParsePolicySet(policySetData []byte) (*v1.PolicySet, error) {
 	set := &v1.PolicySet{}
 	err := protojson.UnmarshalOptions{}.Unmarshal(policySetData, set)
 	if err != nil {
@@ -56,175 +46,4 @@ func (dpi *defaultParserImplementation) ParsePolicySet(policySetData []byte) (*v
 		}
 	}
 	return set, nil
-}
-
-// FetchReferences
-func (dpi *defaultParserImplementation) FetchReferences(fetcher PolicyFetcher, set *v1.PolicySet) (*policyStore, error) {
-	store := &policyStore{}
-	// Fetch the PolicySet common references
-	if set.GetReferences() != nil {
-		for i, ref := range set.GetReferences() {
-			if err := fetchRefIfNeeded(fetcher, store, ref); err != nil {
-				return nil, fmt.Errorf("getting global policy reference #%d: %w", i, err)
-			}
-		}
-	}
-
-	// The fetch external references at the policy level
-	for i, p := range set.Policies {
-		// If the policy has no source, it's inline. Skip
-		if p.GetSource() == nil || p.GetSource().GetLocation() == nil {
-			continue
-		}
-
-		if err := fetchRefIfNeeded(fetcher, store, p.GetSource()); err != nil {
-			return nil, fmt.Errorf("getting policy reference #%d: %w", i, err)
-		}
-	}
-	return store, nil
-}
-
-func fetchRefIfNeeded(fetcher PolicyFetcher, store *policyStore, ref *v1.PolicyRef) error {
-	if ref.GetLocation() == nil {
-		// If there is no location, then noop
-		return nil
-	}
-	// Only fetch the blobs we don't already have
-	needToParse := false
-	for algo, val := range ref.GetLocation().GetDigest() {
-		if fr := store.GetFetchedRefByHash(algo, val); fr == nil {
-			needToParse = true
-			logrus.Infof("dont have data for %s:%s", algo, val)
-			break
-		}
-	}
-	if !needToParse {
-		logrus.Infof("Already have remote policy data")
-		return nil
-	}
-
-	// Check if the policy has a DownloadLocation
-	url := ref.GetLocation().GetDownloadLocation()
-	if url == "" {
-		url = ref.GetLocation().GetUri()
-	}
-
-	if url == "" {
-		return errors.New("policy reference has no url to fetch")
-	}
-
-	logrus.Infof("fetching remote policy data from %s", url)
-	data, err := fetcher.Get(url)
-	if err != nil {
-		return fmt.Errorf("fetching policy reference: %w", err)
-	}
-
-	dataHash, fr, err := parseFetchedRef(data)
-	if err != nil {
-		return fmt.Errorf("parsing remote data: %w", err)
-	}
-
-	hshr := hasher.New()
-	verified, err := hshr.VerifyReader(bytes.NewReader(data), hasher.NewHashSet(ref.GetLocation().GetDigest()))
-	if err != nil {
-		return fmt.Errorf("verifying data: %w", err)
-	}
-
-	if !verified {
-		return fmt.Errorf("cannot validate referenced policy data with provided hashes")
-	}
-
-	fr.Digests = ref.GetLocation().GetDigest()
-
-	// This can happen if the ref hash extra hashes not found
-	// in the known artifact
-	if _, ok := (*store)[dataHash]; ok {
-		maps.Copy((*store)[dataHash].Digests, fr.Digests)
-	} else {
-		(*store)[dataHash] = fr
-	}
-	return nil
-}
-
-// parseFetchedRef reads data and returns a fetchedRef with the
-// parsed policy or policy set. If the data is not a policy or policyset
-// or invalid json, then an error is thrown.
-func parseFetchedRef(data []byte) (string, *fetchedRef, error) {
-	unmarshaler := protojson.UnmarshalOptions{
-		DiscardUnknown: false,
-	}
-
-	// Compute the fetched data digest:
-	h := sha256.New()
-	if _, err := h.Write(data); err != nil {
-		return "", nil, fmt.Errorf("hashing data: %w", err)
-	}
-	dataHash := fmt.Sprintf("%x", h.Sum(nil))
-
-	policySingle := &v1.Policy{}
-	policySet := &v1.PolicySet{}
-	policies := []string{}
-
-	if err := unmarshaler.Unmarshal(data, policySingle); err != nil {
-		if !strings.Contains(err.Error(), "unknown field") {
-			return dataHash, nil, fmt.Errorf("unmarshaling Policy: %w", err)
-		}
-		policySingle = nil
-	}
-
-	if err := unmarshaler.Unmarshal(data, policySet); err != nil {
-		if !strings.Contains(err.Error(), "unknown field") {
-			return dataHash, nil, fmt.Errorf("unmarshaling PolicySet: %w", err)
-		}
-		policySet = nil
-	}
-
-	if policySet == nil && policySingle == nil {
-		return dataHash, nil, fmt.Errorf("data is not an AMPEL Policy or PolicySet")
-	}
-
-	if policySingle != nil && policySingle.GetId() != "" {
-		policies = []string{policySingle.GetId()}
-	}
-
-	if policySet != nil {
-		for _, p := range policySet.GetPolicies() {
-			if p.GetId() == "" {
-				continue
-			}
-			policies = append(policies, p.GetId())
-		}
-	}
-
-	return dataHash, &fetchedRef{
-		Data:      &data,
-		Policy:    policySingle,
-		PolicySet: policySet,
-		Policies:  policies,
-	}, nil
-}
-
-// CompletePolicySet
-func (dpi *defaultParserImplementation) CompletePolicySet(set *v1.PolicySet, store *policyStore) error {
-	for i, p := range set.Policies {
-		// If the policy does not hava remote source, skip
-		if p.Source == nil {
-			continue
-		}
-		remotePolicy, err := store.GetReferencedPolicy(p.Source)
-		if err != nil {
-			return fmt.Errorf("getting referenced policy: %w", err)
-		}
-
-		if remotePolicy == nil {
-			return fmt.Errorf("unable to complete policy #%d, reference nor resolved", i)
-		}
-
-		// Merge the local policy changes onto the remote:
-		proto.Merge(remotePolicy, p)
-
-		// Now replace the local in the policy set with the enriched remote
-		set.Policies[i] = remotePolicy
-	}
-	return nil
 }
--- pkg/policy/parser.go
+++ pkg/policy/parser.go
@@ -4,16 +4,9 @@
 package policy
 
 import (
-	"bytes"
 	"errors"
 	"fmt"
-	"net/url"
 	"os"
-	"strings"
-
-	"github.com/carabiner-dev/vcslocator"
-	"sigs.k8s.io/release-utils/http"
-	"sigs.k8s.io/release-utils/util"
 
 	v1 "github.com/carabiner-dev/ampel/pkg/api/v1"
 )
@@ -28,58 +21,18 @@ const (
 
 var ErrUnsupportedLocationURI = errors.New("unsupported policy location")
 
+// NewParser creates a new policy parser
 func NewParser() *Parser {
 	return &Parser{
-		Fetcher: NewFetcher(),
-		impl:    &defaultParserImplementation{},
+		impl: &defaultParserImplementationV1{},
 	}
 }
 
+// Parser implements methods to read the policy and policy set json files.
+// Note that the parser only deals with decoding json. Use the policy compiler
+// to assemble policies with external/remote references.
 type Parser struct {
-	Fetcher PolicyFetcher
-	impl    parserImplementation
-}
-
-// OpenPolicySet opens a policy set file from a file or a remote location (git/https)
-func (p *Parser) OpenPolicySet(pathUri string) (*v1.PolicySet, error) {
-	// First, try a local file:
-	if util.Exists(pathUri) {
-		return p.ParseFile(pathUri)
-	}
-	// if its not a file, open it from a remote location:
-	return p.OpenRemotePolicySet(pathUri)
-}
-
-// OpenRemoteSet opens a policy set from a remote location
-func (p *Parser) OpenRemotePolicySet(uri string) (*v1.PolicySet, error) {
-	// Check the string, at this point we know it is not a local file
-	if strings.Contains(uri, "git+https:") || strings.Contains(uri, "git+ssh:") {
-		var b bytes.Buffer
-		if err := vcslocator.CopyFile(uri, &b); err != nil {
-			return nil, fmt.Errorf("opening policy from git: %w", err)
-		}
-		return p.ParseSet(b.Bytes())
-	}
-
-	// Next, try to se if its a URL
-	u, err := url.Parse(uri)
-	// If we could not parse it, then we're done
-	if err != nil {
-		return nil, ErrUnsupportedLocationURI
-	}
-
-	// We only support https policies for now
-	if u.Scheme != "https" {
-		return nil, ErrUnsupportedLocationURI
-	}
-
-	// Fetch the data
-	data, err := http.NewAgent().Get(uri)
-	if err != nil {
-		return nil, fmt.Errorf("HTTP error opening remote policy: %w", err)
-	}
-
-	return p.ParseSet(data)
+	impl parserImplementation
 }
 
 // ParseFile parses a policy file
@@ -93,24 +46,12 @@ func (p *Parser) ParseFile(path string) (*v1.PolicySet, error) {
 	return p.ParseSet(data)
 }
 
-// ParseSet parses a policy set
+// ParseSet parses a policy set.
 func (p *Parser) ParseSet(policySetData []byte) (*v1.PolicySet, error) {
 	// Parse the policy set data
 	set, err := p.impl.ParsePolicySet(policySetData)
 	if err != nil {
 		return nil, fmt.Errorf("parsing policy source: %w", err)
 	}
-
-	// Fetch the remote policies referenced in the set to complete it
-	store, err := p.impl.FetchReferences(p.Fetcher, set)
-	if err != nil {
-		return nil, fmt.Errorf("fetching remote references: %w", err)
-	}
-
-	// Complete the PolicySet
-	if err := p.impl.CompletePolicySet(set, store); err != nil {
-		return nil, fmt.Errorf("completing policy set: %w", err)
-	}
-
 	return set, nil
 }
