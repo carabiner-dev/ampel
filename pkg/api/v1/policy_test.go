9971847221d38ef6bd3a50ea4085bd2eaffad1a9
Adolfo García Veytia (Puerco) <puerco@carabiner.dev>
2025-05-09 22:32:45 -0600

    PolicyRef Validate unit test
    Signed-off-by: Adolfo García Veytia (Puerco) <puerco@carabiner.dev>

---
+++ pkg/api/v1/policy_test.go
@@ -0,0 +1,63 @@
+// SPDX-FileCopyrightText: Copyright 2025 Carabiner Systems, Inc
+// SPDX-License-Identifier: Apache-2.0
+
+package v1
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestPolicyRefValidate(t *testing.T) {
+	t.Parallel()
+	for _, tc := range []struct {
+		name    string
+		mustErr bool
+		ref     *PolicyRef
+	}{
+		{
+			"http-requires-hash", true,
+			&PolicyRef{
+				Location: &ResourceDescriptor{
+					Uri: "http://example.com",
+				},
+			},
+		},
+		{
+			"vcslocator-requires-hash", true,
+			&PolicyRef{
+				Location: &ResourceDescriptor{
+					Uri: "git+http://github.com/example",
+				},
+			},
+		},
+		{
+			"vcslocator-with-digest", false,
+			&PolicyRef{
+				Location: &ResourceDescriptor{
+					Uri:    "git+http://github.com/example",
+					Digest: map[string]string{"sha256": "2347962367823768"},
+				},
+			},
+		},
+		{
+			"vcslocator-with-commit", false,
+			&PolicyRef{
+				Location: &ResourceDescriptor{
+					Uri: "git+http://github.com/example@59c8563ff26810478b6ab8ff4c779b4e14385392",
+				},
+			},
+		},
+	} {
+		t.Run(tc.name, func(t *testing.T) {
+			t.Parallel()
+			err := tc.ref.Validate()
+			if tc.mustErr {
+				require.Error(t, err)
+				return
+			}
+			require.NoError(t, err)
+		})
+	}
+}
