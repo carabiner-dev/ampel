df76f361d93dbd5b47de23225e52a5f16144b0d7
Adolfo García Veytia (Puerco) <puerco@carabiner.dev>
2025-05-09 22:29:08 -0600

    Initial PolicyRef Validate()
    Signed-off-by: Adolfo García Veytia (Puerco) <puerco@carabiner.dev>

--- pkg/api/v1/policy.go
+++ pkg/api/v1/policy.go
@@ -4,7 +4,11 @@
 package v1
 
 import (
+	"errors"
 	"fmt"
+	"strings"
+
+	"github.com/carabiner-dev/vcslocator"
 )
 
 func (meta *Meta) testsControl(ctrl *Control) bool {
@@ -52,4 +56,34 @@ func (i *Identity) Slug() string {
 	default:
 		return ""
 	}
+}
+
+// Validate returns an error if the reference is not valid
+func (ref *PolicyRef) Validate() error {
+	errs := []error{}
+	// If the download URL is not a VCS locator, the policy MUST have at least one hash
+	if ref.GetLocation() != nil {
+		uri := ref.GetLocation().GetUri()
+		if uri == "" {
+			uri = ref.GetLocation().GetDownloadLocation()
+		}
+
+		// Ensure a remote reference hash a hash or digest
+		if len(ref.GetLocation().GetDigest()) == 0 {
+			// VCS locators can have a commit or a hash
+			if strings.HasPrefix(uri, "git+") {
+				l := vcslocator.Locator(uri)
+				parts, err := l.Parse()
+				if err != nil {
+					errs = append(errs, fmt.Errorf("parsing VCS locator: %w", err))
+				} else if parts.Commit == "" {
+					errs = append(errs, errors.New("remoter policies referenced by VCS locator require a digest or commit hash"))
+				}
+
+			} else if uri != "" {
+				errs = append(errs, errors.New("remote policies referenced by URL require at least one hash"))
+			}
+		}
+	}
+	return errors.Join(errs...)
 }
